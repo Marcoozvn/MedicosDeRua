e194b68cda470bab394428b5b56b602de69c33b3
Jonathan Neves <jonathanneves.cg@gmail.com>
2020-04-07 19:59:55 -0300

    Co-authored-by: marcos.v.cesar@accenture.com <marcos.v.cesar@accenture.com>

--- MedicosDeRua/src/app/cadastro/cadastro-retorno/cadastro-retorno.component.ts
+++ MedicosDeRua/src/app/cadastro/cadastro-retorno/cadastro-retorno.component.ts
@@ -8,6 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class CadastroRetornoComponent implements OnInit {
 
   constructor() { }
+  drogas = [] = [{ nome: 'Cigarro' }, { nome: 'Cocaína' }, { nome: 'Macohnha' }, { nome: 'LSD' }, { nome: 'Ecstasy' }, { nome: 'Lança perfume/cola' }]
+  exames = ['Destro', 'Parcial de Urina', 'Hemograma', 'Ureia + Creatina', 'TGO + TGP', 'Bilirrubinas', 'Lipidograma', 'Hb glicada']
+  testes = [] = ['Sífilis', 'HIV', 'Hepatite B', 'Hepatite C', 'Gravidez']
+  
+  encaminhamentos = [] = [{
+    area: 'Odontologia',
+    situacao1: 'Não quis/ foi necessário atendimento',
+    situacao2: 'Atendido - ver ficha específica'
+  },{
+    area: 'Psicologia',
+    situacao1: 'Não quis/ foi necessário atendimento',
+    situacao2: 'Atendido - ver ficha específica'
+  },{
+    area: 'Jurídico',
+    situacao1: 'Não quis/ foi necessário atendimento',
+    situacao2: 'Atendido - ver ficha específica'
+  }]
 
   ngOnInit() {
   }
