10e37e5c70d92e7a7c5cbdfe7d1d58ace1a0d0d3
Jonathan Neves <jonathanneves.cg@gmail.com>
2020-04-13 18:06:34 -0300

    continucação do form de anamnese geral

--- MedicosDeRua/src/app/register/anamnese-geral/anamnese-geral.component.ts
+++ MedicosDeRua/src/app/register/anamnese-geral/anamnese-geral.component.ts
@@ -10,13 +10,64 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class AnamneseGeralComponent implements OnInit {
 
   anamneseForm: any;
-  docsPessoais = [] = ['RG', 'CPF', 'Cart.Trab', 'CNH', 'Cert.Nasc', 'Cert.Cas']
-  constructor( private fb: FormBuilder) { }
+  docsPessoais = [] = ['RG', 'CPF', 'Cart.Trab', 'CNH', 'Cert.Nasc', 'Cert.Cas'];
+  drogas = [] = [{ nome: 'Cocaína' }, { nome: 'Maconha' }, { nome: 'LSD' }, { nome: 'Ecstasy' }, { nome: 'Lança perfume' }];
+  servicos = [] = ['FAS', 'CREAS', 'Albergue', 'Centro Pop', 'CRAS', 'Internamento', 'CAPS']
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     this.anamneseForm = this.fb.group({
-      
+      paciente: this.fb.group({
+        nome: ['', Validators.required],
+        idade: '',
+        dataNascimento: '',
+        religiao: '',
+        etnia: '',
+        sexo: '',
+        orientacao: '',
+        situacaoConjugal: '',
+        escolaridade: '',
+        qualCursoTec: '',
+        qualCursoSup: '',
+        cidade: '',
+        ultimaCidade: '',
+        temDocsPessoais: '',
+        documentosPessoais: this.fb.group({
+          RG: [false],
+          CPF: [false],
+          'Cart.Trab': [false],
+          CNH: [false],
+          'Cert.Nasc': [false],
+          'Cert.Cas': [false]
+        }),
+        contato: ''
+      }),
+      perfilSocioeconomico: this.fb.group({
+        tempoDeRua: '',
+        ondevive: '',
+        nomeDarua: '',
+        outrosRua: '',
+        comQuemEstaNaRua: '',
+        outrosCompainha: '',
+        seAcompanhado: this.fb.group({
+          numeroAdultos: '',
+          numeroCriancas: '',
+          ondeFicam:'',
+          estudam: ''
+        }),
+        parentesEmCuritiba: '',
+        localidadeParentes: '',
+        temContato: '',
+        renda:'',
+        carteiraAssinada: '',
+        ocupacaoClt: ''
+      })
     })
+  }
+
+  submitForm() {
+    console.log('Form: ', this.anamneseForm.value);
   }
 
 }
