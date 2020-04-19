5d35ae020f173537b617ed1c6c9eadf81f273e5f
Marcos Cesar <marcos.v.cesar@accenture.com>
2020-04-19 17:15:32 -0300

    Retirando mock

--- MedicosDeRua/src/app/list-users/list-users.component.ts
+++ MedicosDeRua/src/app/list-users/list-users.component.ts
@@ -10,11 +10,7 @@ import { FormService } from '../shared/form.service';
   styleUrls: ['./list-users.component.scss']
 })
 export class ListUsersComponent implements OnInit {
-  /*
-  users = [
-    {_id: '5e87b0825b511f2f7c414d16', nome: 'Marcos Vinicius', cpf: '000.000.000-00', dataNascimento: '07/03/1997'}
-  ];
-  */
+
  users = [];
 
   constructor(private route: Router,
@@ -25,19 +21,15 @@ export class ListUsersComponent implements OnInit {
 
   ngOnInit() {
     this.userService.getUsers().subscribe(res => {
-      console.log(this.users);
-      /*
       this.users = res;
 
       if (!this.users) {
         this.route.navigate(['/app']);
       }
-      */
     });
   }
 
   searchForm(user: any) {
-    console.log(user);
     this.listUsersService.findForms(user._id).subscribe( res => {
       this.formService.setForms(res);
       this.route.navigate(['../forms'], { relativeTo: this.activatedRoute });
